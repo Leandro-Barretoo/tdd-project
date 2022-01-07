2451cbf19ee6ce1ff34f000797eeed69f757d25a
Leandro-Barretoo <officialdevmi@gmail.com>
2022-01-07 14:51:09 +0000

    Add logic in get_factorial to pass test

--- main.rb
+++ main.rb
@@ -6,5 +6,10 @@ class Solver
     def get_factorial(number)
         raise "Number is less than 0" if number.negative?
         return 1 if number.zero?
+        product = 1
+        (1..number).each do |i|
+            product *= i
+        end
+        product
     end
 end
