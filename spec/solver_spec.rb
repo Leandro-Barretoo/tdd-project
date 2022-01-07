279a8a0b9db54c16adc6a21790c0f3d3406d4945
Hector <vv96rca@gmail.com>
2022-01-07 20:38:20 +0530

    add test cases for fizz buzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,10 +7,17 @@ describe Solver do
         number = 5
         number_two = -3
         number_three = 0
+
+        context 'Test the reverse String method in Solver Class' do
+
         it 'should reverse a string' do
             reversed = solve.reverse_name(name)
             expect(reversed).to eq('ordnael')
         end
+    end
+
+
+        context 'Test the factorial method in Solver Class' do
 
         it 'should return the factorial of a given positive number' do
             factorial = solve.get_factorial(number)
@@ -25,5 +32,28 @@ describe Solver do
             factorial_zero = solve.get_factorial(number_three)
             expect(factorial_zero).to eq(1)
         end
+    end
+
+
+        context 'Test the fizzbuzz method in Solver Class' do
+
+            it 'should return fizzbuzz if 15 is passed as number' do
+             string = solve.fizzbuzz(15)
+              expect(string).to eql 'fizzbuzz'
+         end
+         it 'should return fizz if 9 is passed as number' do
+            string = solve.fizzbuzz(9)
+            expect(string).to eql 'fizz'
+          end
+      
+          it 'should return buzz if 10 is passed as number' do
+            string = solve.fizzbuzz(10)
+            expect(string).to eql 'buzz'
+          end
+      
+          it 'should return 7 as a string it is passed as number' do
+            string = solve.fizzbuzz(7)
+            expect(string).to eql '7'
+          end
     end
 end
